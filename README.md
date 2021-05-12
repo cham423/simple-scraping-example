e594ae0a7de7b6f2aec99b54b43b2d8eee84f079
cham423 <cham423@gmail.com>
2021-05-11 22:47:10 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+## Repo Overview
+This is a simple example of a web scraping script, designed to accompany my Source Zero Con talk which will be on May 27th, 2021 at 13:00 Eastern Time. I hope to see you there!
+
+## Script Overview
+I built this script to scrape a tattoo artist's web page, determine if their books are open or closed, and store the results in a json object. It also notifies me using pushover if the web content changes, the program exits, or if the books are determined to be open.
+
+Although this script is not really intended to be used outside of an example, it can do the following:
+	- Scrape a page using requests
+	- Extract paragraph divs with BeautifulSoup
+	- Generate hash of the paragraph content with hashlib
+	- Use regex to determine if books are open or closed
+	- Create json object with relevant attributes, including timestamp, content hash, and book state
+	- Index JSON object to Elasticsearch Index
+	- Depending on results, send push notification using [Pushover](https://pushover.net/)
+		- the script will notify the user if the content hash changes, if the books are open or if there is an error causing the script to exit.
+
+## Usage
+	- `git clone repo`
+	- `pip3 install -r requirements.txt`
+	- fill out required parameters, including:
+		- `url` to scrape
+		- `elasticHost` the Elasticsearch Endpoint to Log to 
+		- `indexName` which will be the index created to store all the results records
+		- `pushoverToken` your pushover app token
+		- `pushoverKey` your pushover user key
+	- `python3 scrape.py`
+
+## Limitations
+	- This script is designed for one very specific use case, scraping [this](https://www.jaxntheboxtattoos.com/) page
+	- The script is not really modular, everything runs in a while loop
+	- Error handling is pretty limited 
---
+++ scrape.py
@@ -0,0 +1,97 @@
+import logging
+import requests
+from bs4 import BeautifulSoup
+import re
+import time
+from datetime import datetime
+import json
+import hashlib
+from elasticsearch import Elasticsearch
+
+# configure logging. debug is loud, info is less loud, error is bad stuff only.
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger("simple-scraping-example")
+
+# you will need to fill this stuff out to get the program to function
+url = ''
+elasticHost = ''
+indexName = 'dev_scrape'
+pushoverToken = ''
+pushoverKey = ''
+sleepDuration = 300
+
+
+# set the user agent header to a real browser
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
+
+# set initial hash to none, so we don't accidentally send a notification the first time we check it
+lastHash = None
+
+# create Elasticsearch object
+es = Elasticsearch(hosts=elasticHost)
+# create index if it doesn't exist already, ignore a 400 error which indicates that it does already
+es.indices.create(index=indexName, ignore=400)
+
+# create a class for the date you intend to scrape. this makes converting to JSON much easier
+class ShopState:
+    def __init__(self, timestamp, isOpen, contentHash):
+        self.timestamp = timestamp
+        self.isOpen = isOpen
+        self.contentHash = contentHash
+
+def sendPushoverNotification(message):
+    try:
+        r = requests.post("https://api.pushover.net/1/messages.json", data = {"token": pushoverToken, "user": pushoverKey, "message": message})
+    except requests.exceptions.RequestException as e:
+        logger.error("error sending pushover notification: {}".format(e)) 
+
+# main loop
+while True:
+    # set the current timestamp to now
+    checkTime = datetime.now().isoformat()
+
+    # make the request to the page
+    try:
+        response = requests.get(url, headers=headers)
+        # parse the downloaded homepage
+        soup = BeautifulSoup(response.text, "lxml")
+        # hash the content from the div class=paragraph objects.
+        # you will likely have to adjust this to a different HTML attribute, depending on the content you are trying to scrape
+        # this hash is useful to validate whether the content changed or not, since we are really only looking for specific strings
+        # in general you should build in failsafes, because regex isn't always reliable, and web content changes a lot
+        contentHash = hashlib.sha256(str(soup.findAll('div',attrs={"class":"paragraph"})).encode('utf-8')).hexdigest()
+        # compare lasthash to current hash, then send a notification if it changes
+        if (lastHash is not None) and (contentHash != lastHash):
+            sendPushoverNotification("site content changed, check {}".format(url))
+        # set the last hash to the current hash, so we can compare it next time.
+        lastHash = contentHash
+
+        # search for strings in the content with regex.
+        # if the string is found at least once, we assume the books are closed 
+        if len(soup.findAll(string=re.compile("books closed", re.IGNORECASE))) > 0:
+            isOpen = False
+        # if the string isn't found at least once, let's check if the books are open
+        elif len(soup.findALL(string=re.compile("books open", re.IGNORECASE))) > 0:
+            isOpen = True
+        # if neither string is found, either the website is down or the content of the page has changed enough that we need to rework our logic, so we exit
+        else:
+            # send notification so we know that something went wrong
+            sendPushoverNotification("Something is up, scraper exiting, check {}".format(url))
+            sys.exit("couldn't determine state, exiting")
+
+        # now that we have data, let's do something with it
+        currentState = ShopState(checkTime, isOpen, contentHash)
+        # log the actual json info (optional)
+        logger.info("{}".format(json.dumps(currentState.__dict__)))
+        # index to elasticsearch
+        try:
+            es.index(index=indexName,body=json.dumps(currentState.__dict__))
+        except Exception as e:
+            logger.error("error indexing to elasticsearch: {}".format(e))
+
+    except Exception as e:
+        logger.error('error in main loop: {}'.format(e))
+        continue
+
+    # sleep for the interval specified (default 5 minutes)
+    time.sleep(sleepDuration)
